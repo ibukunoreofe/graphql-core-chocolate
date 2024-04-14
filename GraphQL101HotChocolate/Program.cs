3f2f0c942b89847d16deca9aa13f1a61cba45939
Ibukun Bello <ibukunoreofe@gmail.com>
2024-04-14 17:25:13 +0200

    Improved Docs

--- GraphQL101HotChocolate/Program.cs
+++ GraphQL101HotChocolate/Program.cs
@@ -7,7 +7,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPooledDbContextFactory<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    //.LogTo(Console.WriteLine)
+    );
 
 // Configure more services
 // Add services to the container.
--- GraphQL101HotChocolate/Schema/Queries/Query.cs
+++ GraphQL101HotChocolate/Schema/Queries/Query.cs
@@ -31,12 +31,66 @@ namespace GraphQL101HotChocolate.Schema.Queries
         /// </summary>
         /// <param name="contextFactory"></param>
         /// <returns></returns>
+        /// 
+        /// query
+        //{
+        //    users(first: 3, after: "MA=="){
+        //        edges{
+        //            node{
+        //                id
+        //                username
+        //                email
+        //                createdDate
+        //            }
+        //            cursor
+        //        }
+        //        pageInfo{
+        //            startCursor
+        //            endCursor
+        //        }
+        //    }
+        //}
+        /// 
+        /// 
+        /// ORDER is important: UseDbContext -> UsePaging -> UseProjection -> UseFiltering -> UseSorting
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 2)]  
         [UseProjection]
+        [UseSorting]
         public IQueryable<User> GetUsers([Service] IDbContextFactory<AppDbContext> contextFactory)
         {
             var context = contextFactory.CreateDbContext();
             return context.Users;
         }
+
+        /// <summary>
+        /// query
+            //{
+            //   offsetUsers (skip:1 , take: 2){
+            //        items {
+            //            id
+            //            username
+            //            email
+            //            createdDate
+            //        }
+            //        pageInfo{
+            //            hasNextPage
+            //            hasPreviousPage
+            //        }
+            //        totalCount
+            //    }
+            //}
+        /// </summary>
+        /// <param name="contextFactory"></param>
+        /// <returns></returns>
+        [UseOffsetPaging(IncludeTotalCount = true, DefaultPageSize = 2)]
+        [UseProjection] // Without this, it will select all columns in the table
+        [UseFiltering]
+        public IQueryable<User> GetOffsetUsers([Service] IDbContextFactory<AppDbContext> contextFactory)
+        {
+            var context = contextFactory.CreateDbContext();
+            return context.Users.OrderBy(x => x.Id);
+        }
+
 
         [GraphQLDeprecated("It was just for testing")]
         public string Hello() => "world";
